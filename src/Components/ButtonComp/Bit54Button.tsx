cb88322d1a2ad45a4c7a94da975e736736579037
Heaven <gabrielheavenlydawn@gmail.com>
2024-09-14 16:00:34 +0100

    chore: header component done

--- src/Components/ButtonComp/Bit54Button.tsx
+++ src/Components/ButtonComp/Bit54Button.tsx
@@ -4,8 +4,8 @@ import DollarIcon from "../../../public/DollorIcon.png";
 const Bit54Button = () => {
   return (
     <div>
-      <button className="bg-[#F7931A] py-3 px-6">
-        <p className="text-2xl font-bold">Send Now</p>
+      <button className="flex items-center gap-2 bg-[#F7931A] py-4 px-6 rounded-lg">
+        <p className="text-2xl font-bold text-white">Send Now</p>
         <Image src={DollarIcon} width={20} height={20} alt="dollar sign" />
       </button>
     </div>
--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,11 +1,32 @@
-import React from 'react'
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+import Bit54Logo from "../../../public/Bit54Logo.png";
+import Bit54Button from "../ButtonComp/Bit54Button";
 
 const Header = () => {
   return (
-    <div>
-        
-    </div>
-  )
-}
+    <div className="flex items-center justify-between">
+      <div>
+        <Image src={Bit54Logo} alt="Logo" height={150} width={150} />
+      </div>
 
-export default Header
+      <div>
+        <ul className=" flex items-center gap-8 font-medium">
+          <Link href="/">
+            <li>Home</li>
+          </Link>
+          <Link href="/">
+            <li>FAQs</li>
+          </Link>
+          <Link href="/">
+            <li>Contact Us</li>
+          </Link>
+        </ul>
+      </div>
+      <Bit54Button />
+    </div>
+  );
+};
+
+export default Header;
