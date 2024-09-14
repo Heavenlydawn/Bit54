d57f829024196ebe3277dd4af1c24b0020830414
Heaven <gabrielheavenlydawn@gmail.com>
2024-09-14 13:42:06 +0100

    chore: Footer Section Done

--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -1,9 +1,18 @@
-import React from 'react'
-
+import React from "react";
+import Image from "next/image";
+import Bit54Logo from "../../../public/Bit54Logo.png";
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer className="flex items-center justify-between">
+      <div>
+        <Image src={Bit54Logo} alt="Bit54" width={150} height={150} />
+        <p className="text-[#797979] text-lg mt-4">Enjoy quick and cheaper transfers <br />to any country in Africa</p>
+      </div>
+      <div>
+        <p className="text-[#797979] text-lg">hello@bit54.com</p>
+      </div>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
