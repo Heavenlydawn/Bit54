9e783159504a27ffb5f74cae3d495903967ee8e9
Jeremy Ikwuje <ikwuje24@gmail.com>
2024-09-16 14:00:49 +0100

    feat: update wrangler for cloudflare

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,4 @@
-import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
-
-
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: { runtime: 'edge'}
-};
-
-if (process.env.NODE_ENV === 'development') {
-  await setupDevPlatform();
-}
+const nextConfig = {};
 
 export default nextConfig;
